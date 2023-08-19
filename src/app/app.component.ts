065e6e510b28053ac1f48b2eb44e8b6911a01041
RituBhowmik <98028967+RituBhowmik@users.noreply.github.com>
2023-08-19 16:43:08 +0200

    Comments feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+export type comment = {
+  User: string;
+  comment: string;
+  date: string;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  public comment:comment = { User: '', comment: '', date: ''}
+  public allComments: any;
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:3000/allcomments').subscribe((data) => {
+      this.allComments = data;
+    });
+  }
   title = 'HotMeals';
 }
